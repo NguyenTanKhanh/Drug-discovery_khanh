622bf41e555cc6f189aa435225c8d61e795eaa48
NguyenTanKhanh <63031249+NguyenTanKhanh@users.noreply.github.com>
2021-01-24 04:00:02 +0700

    Update vina_screen_local.sh

--- vina_screen_local.sh
+++ vina_screen_local.sh
@@ -2,5 +2,5 @@ for f in ./ligands/*.pdbqt; do
     b=`basename $f .pdbqt`
     echo Processing ligand $b
     mkdir -p $b
-    vina --config conf.txt--receptor ./protein/bovineXOR.pdbqt--ligand $f --out ./output/${b}/out.pdbqt >> docking_result_output.txt
+    vina --config conf.txt--ligand $f >> docking_result_output.txt
 done

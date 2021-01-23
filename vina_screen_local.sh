4a7782b72078b61afff26f7e1e68be7c3ba6de6a
NguyenTanKhanh <63031249+NguyenTanKhanh@users.noreply.github.com>
2021-01-24 04:11:18 +0700

    Update vina_screen_local.sh

--- vina_screen_local.sh
+++ vina_screen_local.sh
@@ -4,5 +4,5 @@ for f in *.pdbqt; do
     b=`basename $f .pdbqt`
     echo Processing ligand $b
     mkdir -p $b
-    vina --config conf.txt --ligand $f --out ${b}/out.pdbqt >> docking_result_output.txt
+    vina --config conf.txt --ligand $f --out ${b}/out.pdbqt --log ${b}/log.txt
 done

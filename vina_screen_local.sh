7f86cb605cc8aade3d76c8e966ed046a43500ed2
NguyenTanKhanh <63031249+NguyenTanKhanh@users.noreply.github.com>
2021-01-24 03:33:21 +0700

    Add files via upload

---
+++ vina_screen_local.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for ligands in $(ls -1 ./ligands/)
+do
+    vina --receptor ./protein/bovineXOR.pdbqt --ligand ./ligands/$ligand --our ./output/ --config config.txt >> docking_result_output.txt
+done

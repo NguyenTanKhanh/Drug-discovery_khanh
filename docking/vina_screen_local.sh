d829ef9051c1c00d0bf97f54acbe14c2abea2ae0
NguyenTanKhanh <63031249+NguyenTanKhanh@users.noreply.github.com>
2021-01-28 09:20:01 +0700

    Delete vina_screen_local.sh

--- docking/vina_screen_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for ligands in $(ls -1 ./ligands/)
-do
-    !./vina --receptor ./protein/bovineXOR.pdbqt --ligand ./ligands/$ligand --out ./output/ --config config.txt >> docking_result_output.txt
-done
